98349d7cedc8666fc6a9a5a23c334bc2e9855910
superfantasticawesome <hello@superfantasticawesome.com>
2014-03-09 10:39:26 -0400

    cleanup

--- bootstrapper.ps1
+++ bootstrapper.ps1
@@ -16,39 +16,11 @@ Invoke-Expression 'cmd /C ping google.com' *> $null
 # Get the temporary folder environment variable
 $temp = [System.Environment]::GetEnvironmentVariable('TEMP')
 
-# Create a working directory
-$setup = "$temp\setup"
-if ( (Test-Path "$temp") -And !(Test-Path $setup) ) { 
-  New-Item -type directory "$setup"
-} 
-
 # Get the system drive environment variable
 $systemdrive = [System.Environment]::GetEnvironmentVariable('SYSTEMDRIVE')
 
 # Get program files environment variable
 $programfiles = [System.Environment]::GetEnvironmentVariable('PROGRAMFILES')
-
-# Set the PATH environment variable for Chocolatey & VMware Tools
-[System.Environment]::SetEnvironmentVariable("PATH", $Env:Path + ";$programfiles\VMware\VMware Tools;$systemdrive\chocolatey\bin", "Machine")
-
-# Get the URI to the latest Windows x64 VMtools release
-$latest = (Invoke-WebRequest -UseBasicParsing -Uri "http://packages.vmware.com/tools/esx/5.5p01/windows/x64/index.html").Links | 
-Where-Object {$_.href.EndsWith('exe')} | Select -Expand href
-
-# Download VMware Tools
-Import-Module BitsTransfer
-Start-BitsTransfer -Source "http://packages.vmware.com/tools/esx/5.5p01/windows/x64/$latest" -Destination "$setup\vmtools.exe"
-
-# If the download was successful, install VMware Tools
-if ( Test-Path "$setup\vmtools.exe" ) { 
-  Invoke-Expression "$setup\vmtools.exe /S /v '/qn REBOOT=R ADDLOCAL=ALL'"
-}
-
-# Capture OVF runtime environment metadata
-if ( Test-Path "$programfiles\VMware\VMware Tools\vmtoolsd.exe" ) {
-  Set-Location "$programfiles\VMware\VMware Tools"
-  Invoke-Command { & cmd /C 'vmtoolsd.exe --cmd "info-get guestinfo.ovfEnv"' } | Add-Content -Encoding UTF8 "$setup\ovf-env.xml"
-}
 
 # Install Chocolatey
 Invoke-Expression (New-Object System.Net.Webclient).DownloadString('http://www.chocolatey.org/install.ps1')
@@ -56,44 +28,75 @@ Invoke-Expression (New-Object System.Net.Webclient).DownloadString('http://www.c
 # Install Puppet with Chocolatey
 Invoke-Expression "cmd /C $systemdrive\chocolatey\bin\cinst puppet"
 
-# Do the XSL transform
-if ( (Test-Path "$systemdrive\ProgramData\PuppetLabs\Facter\facts.d") -and (Test-Path "$setup\ovf-env.xml") ) {
-  $yaml = "$systemdrive\ProgramData\PuppetLabs\Facter\facts.d\facts.yaml"
-  $xsl = New-Object System.Xml.Xsl.XslCompiledTransform
-  $xsl.Load([xml](New-Object System.Net.WebClient).DownloadString("https://raw.github.com/superfantasticawesome/provisioning/master/xml-to-yaml.xsl"))
-  $xsl.Transform("$setup\ovf-env.xml", $yaml)
-  if ( Test-Path $yaml ) {
-    # Re-write the file without the Unicode Byte Order Marker (BOM)
-    $facts = Get-Content $yaml
-    [System.IO.File]::WriteAllLines($yamfile, $facts)
+$virtual = Invoke-Expression "cmd /C facter virtual"
+
+if ( $virtual -eq "vmware" ) {
+  # Create a working directory
+  $setup = "$temp\setup"
+  if ( (Test-Path "$temp") -And !(Test-Path $setup) ) { 
+    New-Item -type directory "$setup"
+  } 
+
+  # Set the PATH environment variable for VMware Tools
+  [System.Environment]::SetEnvironmentVariable("PATH", $Env:Path + ";$programfiles\VMware\VMware Tools", "User")
+
+  # Get the URI to the latest Windows x64 VMtools release
+  $latest = (Invoke-WebRequest -UseBasicParsing -Uri "http://packages.vmware.com/tools/esx/5.5p01/windows/x64/index.html").Links | 
+  Where-Object {$_.href.EndsWith('exe')} | Select -Expand href
+
+  # Download VMware Tools
+  Import-Module BitsTransfer
+  Start-BitsTransfer -Source "http://packages.vmware.com/tools/esx/5.5p01/windows/x64/$latest" -Destination "$setup\vmtools.exe"
+
+  # If the download was successful, install VMware Tools
+  if ( Test-Path "$setup\vmtools.exe" ) { 
+    Invoke-Expression "$setup\vmtools.exe /S /v '/qn REBOOT=R ADDLOCAL=ALL'"
   }
-}
 
-# Rename the computer
-# Note that the "keys" array maps directly to my OVF custom properties. 
-# Adjust for your environment as required.
-if ( Test-Path "$setup\ovf-env.xml" ) {
-  $currhostname = Invoke-Expression "cmd /C hostname"
-  $keys = 'app_project', 'app_environment', 'app_role', 'app_id'
-  $xml = New-Object -TypeName XML
-  $xml.Load( "$setup\ovf-env.xml" )
-  $newhostname = $xml.Environment.PropertySection.Property | 
-    % -Begin { $h = @{} } -Process { $h[$_.Key] = $_.Value } -End { ($keys | %{ $h.$_ }) -Join '-' } 
-  if ( "$newhostname" -ne "$currhostname" ) {
-    Rename-Computer -NewName $newhostname -Force
+  # Capture OVF runtime environment metadata
+  if ( Test-Path "$programfiles\VMware\VMware Tools\vmtoolsd.exe" ) {
+    Set-Location "$programfiles\VMware\VMware Tools"
+    Invoke-Command { & cmd /C 'vmtoolsd.exe --cmd "info-get guestinfo.ovfEnv"' } | Add-Content -Encoding UTF8 "$setup\ovf-env.xml"
   }
-}
 
-# Cleanup
-Remove-Item -Recurse -Force "$setup\*"
+  # Do the XSL transform and remove BOM
+  if ( (Test-Path "$systemdrive\ProgramData\PuppetLabs\Facter\facts.d") -and (Test-Path "$setup\ovf-env.xml") ) {
+    $yaml = "$systemdrive\ProgramData\PuppetLabs\Facter\facts.d\facts.yaml"
+    $xsl = New-Object System.Xml.Xsl.XslCompiledTransform
+    $xsl.Load([xml](New-Object System.Net.WebClient).DownloadString("https://raw.github.com/superfantasticawesome/provisioning/master/xml-to-yaml.xsl"))
+    $xsl.Transform("$setup\ovf-env.xml", $yaml)
+    if ( Test-Path $yaml ) {
+      $facts = Get-Content $yaml
+      [System.IO.File]::WriteAllLines($yaml, $facts)
+    }
+  }
 
-# Install WuInstall
-Invoke-Expression "cmd /C $systemdrive\chocolatey\bin\cinst wuinstall"
+  # Rename the computer
+  # Note that the "keys" array maps directly to my OVF custom properties. 
+  # Adjust for your environment as required.
+  if ( Test-Path "$setup\ovf-env.xml" ) {
+    $currhostname = Invoke-Expression "cmd /C hostname"
+    $keys = 'app_project', 'app_environment', 'app_role', 'app_id'
+    $xml = New-Object -TypeName XML
+    $xml.Load( "$setup\ovf-env.xml" )
+    $newhostname = $xml.Environment.PropertySection.Property | 
+      % -Begin { $h = @{} } -Process { $h[$_.Key] = $_.Value } -End { ($keys | %{ $h.$_ }) -Join '-' } 
+    if ( "$newhostname" -ne "$currhostname" ) {
+      Rename-Computer -NewName $newhostname -Force
+    }
+  }
 
-# Run WuInstall 
-Invoke-Expression "cmd /C $systemdrive\chocolatey\bin\cinst wuinstall.run"
+  # Cleanup
+  Remove-Item -Recurse -Force "$setup\*"
 
-# Reboot 
-if ( Test-Path "$systemdrive\ProgramData\PuppetLabs\Facter\facts.d\facts.yaml" ) {
-  Restart-Computer
+  # Install WuInstall
+  Invoke-Expression "cmd /C $systemdrive\chocolatey\bin\cinst wuinstall"
+
+  # Run WuInstall 
+  Invoke-Expression "cmd /C $systemdrive\chocolatey\bin\cinst wuinstall.run"
+
+  # Reboot 
+  if ( Test-Path "$systemdrive\ProgramData\PuppetLabs\Facter\facts.d\facts.yaml" ) {
+    Restart-Computer
+  }
 }
